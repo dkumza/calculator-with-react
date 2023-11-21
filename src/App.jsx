ee75a7717c6c008179b135590bb39c3d775d1db3
Darius <dariuskumza@gmail.com>
2023-11-21 15:51:23 +0200

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+   //  const [count, setCount] = useState(0);
+
+   return (
+      <div className="mt-12 border rounded-xl w-96 mx-auto flex flex-col">
+         <div className="h-36 border rounded-md m-2"></div>
+         <div className="h-72 border rounded-md m-2"></div>
+      </div>
+   );
+}
+
+export default App;
